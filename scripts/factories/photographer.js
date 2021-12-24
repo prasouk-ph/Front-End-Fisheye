d2dfa17b1ec853e8ed1f010d32b5c13ed4bfc78f
prasouk-ph <prasouk.ph@gmail.com>
2021-12-24 16:18:20 +0100

    Display photographer description and price

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,19 +1,25 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        const h3 = document.createElement( 'h3' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3 = document.createElement( 'h3' );
+        const description = document.createElement( 'p' );
+        const priceContainer = document.createElement( 'p' );
         h2.textContent = name;
         h3.textContent = `${city}, ${country}`;
+        description.textContent = tagline;
+        priceContainer.textContent = price;
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(h3);
+        article.appendChild(description);
+        article.appendChild(priceContainer);
         return (article);
     }
     return { name, picture, getUserCardDOM }
