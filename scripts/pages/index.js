d80aa26932281364ddd31d5804719416f9e2fd8f
prasouk-ph <prasouk.ph@gmail.com>
2021-12-24 20:21:32 +0100

    Photographer content lay-out

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -23,6 +23,7 @@
         // et bien retourner le tableau photographers seulement une fois
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
+            // photographers: [...photographers]})
     }
 
     async function displayData(photographers) {
