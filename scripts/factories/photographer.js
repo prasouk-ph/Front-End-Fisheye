92b488bbab6845e91909c8fe6f6493e6581a28ec
prasouk-ph <prasouk.ph@gmail.com>
2021-12-24 16:05:13 +0100

    Display photographer city

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,19 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const h3 = document.createElement( 'h3' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h3.textContent = city;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
         return (article);
     }
     return { name, picture, getUserCardDOM }
