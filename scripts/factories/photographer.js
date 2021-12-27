3c19de5a6e7f2d6d948d2838a054a335edaba779
prasouk-ph <prasouk.ph@gmail.com>
2021-12-27 18:12:00 +0100

    Fetch photographer from json file

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,10 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
+            const response = await fetch('data/photographers.json');
+            const result = await response.json();
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-            // photographers: [...photographers]})
+            photographers: [...result.photographers]})
     }
-
+      
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
@@ -37,10 +16,9 @@
     };
 
     async function init() {
-        // Récupère les datas des photographes
+        // Récupère les datas des photographes        
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
     
     init();
-    
