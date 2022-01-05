ddc6c45b7beb1d6405e8a9f05d92e62d35208eb5
prasouk-ph <prasouk.ph@gmail.com>
2022-01-05 12:13:06 +0100

    Put factory with index js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -21,5 +21,42 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
+
+    function photographerFactory(data) {
+        const { name, portrait, city, country, tagline, price, id } = data;
+    
+        const picture = `assets/photographers/${portrait}`;
+    
+        function getUserCardDOM() {
+            const article = document.createElement( 'article' );
+            const headerCard = document.createElement( 'div' );
+            headerCard.classList.add("card-header")
+            const img = document.createElement( 'img' );
+            img.setAttribute("src", picture)
+            const cardContent = document.createElement( 'div' );
+            cardContent.classList.add("card-content")
+            const h2 = document.createElement( 'h2' );
+            const h3 = document.createElement( 'h3' );
+            const slogan = document.createElement( 'p' );
+            slogan.classList.add("slogan");
+            const priceContainer = document.createElement( 'p' );
+            const link = document.createElement( 'a' );
+            h2.textContent = name;
+            h3.textContent = `${city}, ${country}`;
+            slogan.textContent = tagline;
+            priceContainer.textContent = `${price}€/jour`;
+            link.href = `photographer.html?id=${id}`;
+            article.appendChild(headerCard);
+            article.appendChild(cardContent);
+            headerCard.appendChild(link);
+            link.appendChild(img);
+            link.appendChild(h2);
+            cardContent.appendChild(h3);
+            cardContent.appendChild(slogan);
+            cardContent.appendChild(priceContainer);
+            return (article);
+        }
+        return { name, picture, getUserCardDOM }
+    }
     
     init();
